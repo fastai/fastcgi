359e8dc78efe21ca5cc44ea85b8237a6024f38b7
Jeremy Howard <j@fast.ai>
2020-11-18 10:53:14 -0800

    refactor

--- get_http2fcgi.sh
+++ get_http2fcgi.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+nm=http2fcgi
 case "$OSTYPE" in
   darwin*)  os=darwin; ;;
   linux*)   os=linux; ;;
-  windows*)   os=windows; ;;
+  windows*) os=windows; ;;
   *)        echo "unknown: $OSTYPE" ;;
 esac
 
-[[ -f http2fcgi ]] && exit
-wget --quiet -nc -O - https://github.com/fastai/http2fcgi/releases/latest/download/http2fcgi-${os}-amd64.tgz | tar xz
-chmod u+x http2fcgi
+[[ -f $nm ]] && exit
+wget -qO- https://github.com/fastai/$nm/releases/latest/download/$nm-$os-amd64.tgz | tar xz
+chmod u+x $nm
 

d4f5e7cec74229749df5805a3911e1e9b9a6affd
Jeremy Howard <j@fast.ai>
2020-11-18 10:49:03 -0800

    new http2cgi

--- get_http2fcgi.sh
+++ get_http2fcgi.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+set -e
 
 case "$OSTYPE" in
   darwin*)  os=darwin; ;;
   linux*)   os=linux; ;;
+  windows*)   os=linux; ;;
   *)        echo "unknown: $OSTYPE" ;;
 esac
 
 [[ -f http2fcgi ]] && exit
-wget -nc -O - https://github.com/alash3al/http2fcgi/releases/download/v1.0.0/http2fcgi_${os}_amd64.zip | gunzip - > http2fcgi
+wget --quiet -nc -O - https://github.com/fastai/http2fcgi/releases/latest/download/http2fcgi-${os}-amd64.tgz | tar xz
 chmod u+x http2fcgi
 

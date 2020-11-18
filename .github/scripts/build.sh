dc3ab0bdd0b483a009b7ca695d6b6ab835ee461a
Jeremy Howard <j@fast.ai>
2020-11-18 14:45:21 -0800

    build.sh

---
+++ .github/scripts/build.sh
@@ -0,0 +1,10 @@
+fail() { echo -e "::error::${1}"; exit 1; }
+
+nbdev_read_nbs
+nbdev_clean_nbs
+status=$(git status -uno -s)
+[[ -n "${status}" ]] && fail "Detected unstripped out notebooks.\nRemember to run nbdev_install_git_hooks.\n\n${status}"
+diffs=$(nbdev_diff_nbs)
+[[ -n "${diffs}" ]] && fail "Detected difference between the notebooks and the library.\n\n${diffs}"
+nbdev_test_nbs
+

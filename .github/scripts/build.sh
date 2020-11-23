4d2ac30560b61aa1eceb218bb38364e9159848c4
Jeremy Howard <j@fast.ai>
2020-11-22 17:06:08 -0800

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -8,5 +8,4 @@ status=$(git status -uno -s)
 [[ -n "${status}" ]] && fail "Detected unstripped out notebooks.\nRemember to run nbdev_install_git_hooks.\n\n${status}"
 diffs=$(nbdev_diff_nbs)
 [[ -n "${diffs}" ]] && fail "Detected difference between the notebooks and the library.\n\n${diffs}"
-nbdev_test_nbs
-
+make test
